5ceb3bd8a8a963fd4b06989a67e7770334fdb52a
Hiroyuki MORITA <morita@val.co.jp>
2013-12-03 14:33:44 +0900

    read config data from environment variables

--- management_instance.rb
+++ management_instance.rb
@@ -1,10 +1,11 @@
 require 'bundler/setup'
 Bundler.require
 
-config = YAML.load(File.read("./config/aws.yaml"))
-AWS.config(config)
-
-ec2 = AWS::EC2.new(config)
+ec2 = AWS::EC2.new(
+  access_key_id:     ENV['ACCESS_KEY_ID'],
+  secret_access_key: ENV['SECRET_KEY_ID'],
+  ec2_endpoint:      'ec2.ap-northeast-1.amazonaws.com'
+)
 
     ec2.instances.each do |instance|
         if instance.tags.to_h["type"] == "public_ip_assign"
