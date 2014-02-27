901fdf6536e5802323311609b3ca1eccf3434342
takemasa <takemasa@val.co.jp>
2014-02-27 18:47:55 +0900

    natインスタンスが停止している間、別のインスタンスをroutetableに設定

--- management_instance.rb
+++ management_instance.rb
@@ -7,12 +7,23 @@ ec2 = AWS::EC2.new(
   ec2_endpoint:      'ec2.ap-northeast-1.amazonaws.com'
 )
 
-    ec2.instances.each do |instance|
-        if instance.tags.to_h["type"] == "public_ip_assign"
-                instance.stop
-                puts "stop #{instance.id}"
-    sleep(900)
-                instance.start
-                puts "start #{instance.id}"
-        end
-    end
+nat_instance = nil
+nat_tmp = nil
+
+ec2.instances.each do |instance|
+  if instance.tags.to_h["type"] == "tmp_public_ip_assign"
+    nat_tmp = instance
+    instance.start
+    sleep(300)
+  elsif instance.tags.to_h["type"] == "public_ip_assign"
+    nat_instance = instance
+  end
+end
+
+ec2.client.replace_route(:route_table_id => 'rtb-cd593ba5', :destination_cidr_block => '0.0.0.0/0', :instance_id=>"#{nat_tmp.id}") # インスタンスがpending, stoppingのときはエラーが発生する
+nat_instance.stop
+sleep(900)
+nat_instance.start
+sleep(300)
+ec2.client.replace_route(:route_table_id => 'rtb-cd593ba5', :destination_cidr_block => '0.0.0.0/0', :instance_id=>"#{nat_instance.id}")
+nat_tmp.stop
