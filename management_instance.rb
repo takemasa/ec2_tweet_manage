051c51991e4e86e35b9fa901a979b97792233089
takemasa <takemasa@val.co.jp>
2014-04-02 16:40:19 +0900

    ETL処理を行うインスタンスの起動/停止を設定

--- management_instance.rb
+++ management_instance.rb
@@ -7,23 +7,31 @@ ec2 = AWS::EC2.new(
   ec2_endpoint:      'ec2.ap-northeast-1.amazonaws.com'
 )
 
-nat_instance = nil
+nat = nil
 nat_tmp = nil
-
+etl = nil
 ec2.instances.each do |instance|
-  if instance.tags.to_h["type"] == "tmp_public_ip_assign"
-    nat_tmp = instance
-    instance.start
-    sleep(300)
-  elsif instance.tags.to_h["type"] == "public_ip_assign"
-    nat_instance = instance
+  case instance.tags.to_h["type"]
+  when "redshift_etl"
+    etl = instance # RedshiftへのETL処理を行うインスタンス
+  when "tmp_public_ip_assign"
+    nat_tmp = instance # natの停止中に動作するNATインスタンス
+  when "public_ip_assign"
+    nat = instance # メインで利用されるNATインスタンス
   end
 end
 
+etl.start #etlインスタンスの起動中にcronで設定された処理が行われる
+nat_tmp.start
+sleep(5*60)
+# ルートテーブルをnatからnat_tmpに付け替え
 ec2.client.replace_route(:route_table_id => 'rtb-cd593ba5', :destination_cidr_block => '0.0.0.0/0', :instance_id=>"#{nat_tmp.id}") # インスタンスがpending, stoppingのときはエラーが発生する
-nat_instance.stop
-sleep(900)
-nat_instance.start
-sleep(300)
-ec2.client.replace_route(:route_table_id => 'rtb-cd593ba5', :destination_cidr_block => '0.0.0.0/0', :instance_id=>"#{nat_instance.id}")
+nat.stop
+sleep(15*60)
+nat.start
+sleep(5*60)
+# natの再起動後にルートテーブルを戻すとIPアドレスが変わる
+ec2.client.replace_route(:route_table_id => 'rtb-cd593ba5', :destination_cidr_block => '0.0.0.0/0', :instance_id=>"#{nat.id}")
 nat_tmp.stop
+sleep(60*60)
+etl.stop
